261b0ccd265837916d33134d2b776bb907eafb84
Robilol <robin.regis78@gmail.com>
2023-04-21 15:06:25 +0200

    fix

--- components/RandomMovingSplash.tsx
+++ components/RandomMovingSplash.tsx
@@ -13,8 +13,8 @@ const RandomMovingSplash: FC<RandomMovingSplashProps> = ({width, img, height, le
   const [pos, setPos] = useState<{left: number, top: number}>({left, top})
 
   const getNewPos = () => {
-    const h = window.innerHeight - height;
-    const w = window.innerWidth - width;
+    const h = window.innerHeight - (height / 2);
+    const w = window.innerWidth - (width / 2);
 
     const nh = Math.floor(Math.random() * h);
     const nw = Math.floor(Math.random() * w);
@@ -24,14 +24,14 @@ const RandomMovingSplash: FC<RandomMovingSplashProps> = ({width, img, height, le
 
   useEffect(() => {
     getNewPos()
-    const interval = setInterval(() => getNewPos(), 15000);
+    const interval = setInterval(() => getNewPos(), 10000);
     return () => {
       clearInterval(interval);
     };
   }, []);
 
   return (
-    <div className="relative transition-all duration-[15000ms] ease-linear" style={{
+    <div className="absolute transition-all duration-[10000ms] ease-linear" style={{
       width, height,
       left: pos.left,
       top: pos.top
